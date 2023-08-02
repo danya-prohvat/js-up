7d2b9dacc765ab5192fb71f7e8b4e09231c04790
daniil <prestig.rd85@gmail.com>
2023-08-02 17:25:27 +0300

    title

--- app/flex/page.tsx
+++ app/flex/page.tsx
@@ -1,5 +1,11 @@
 import { Flex } from "@components/contents/Flex";
 import { Layout } from "@components/core/Layout";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Flexbox CSS Cheat Sheet',
+  description: 'Flexbox CSS Cheat Sheet',
+}
 
 export default function FlexPage() {
   return (
--- app/grid/page.tsx
+++ app/grid/page.tsx
@@ -1,5 +1,11 @@
 import { Grid } from "@components/contents/Grid";
 import { Layout } from "@components/core/Layout";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Grid CSS Cheat Sheet',
+  description: 'Grid CSS Cheat Sheet',
+}
 
 export default function GridPage() {
   return (
